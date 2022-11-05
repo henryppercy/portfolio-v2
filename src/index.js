6699de6a04c96c6537ed0a76eeeda3ffffb25a9c
Henry <henryppercy@gmail.com>
2022-11-05 21:57:53 +0000

    Added server-side routing

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+} from 'react-router-dom';
+import ErrorPage from './error-page';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        errorElement: <ErrorPage />,
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
