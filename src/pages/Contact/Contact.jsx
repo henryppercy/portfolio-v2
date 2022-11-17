96e70fa5f976b4802ebdfbcbfb26c5a76eb57fa4
Henry <henryppercy@gmail.com>
2022-11-17 17:06:00 +0000

    Removed link to Slack

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
 
             <main className="contact">
                 <h2>Get In Touch</h2>
-                <p>I’m currently open and looking for work so please get in touch if you have any Junior Developer roles available.</p>
+                <p>I’m currently open and looking for work so please get in touch if you have any Software Developer roles available.</p>
                 <p>The best way to reach me is to send an email but feel free to reach out on LinkedIn.</p>
                 <div className="contact-info">
                     <p>Email: <a href="mailto:henrypercydev@gmail.com">henrypercydev@gmail.com</a></p>
